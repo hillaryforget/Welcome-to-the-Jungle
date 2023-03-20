da34d83a4f3dfa107ead2da7344b6de4ca97f1bb
Hillary Forget <hillaryforget@outlook.com>
2023-03-20 03:04:40 -0400

    order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def empty_cart!
-    # empty hash means no products in cart :)
+    
     update_cart({})
   end
 
@@ -39,7 +39,7 @@ class OrdersController < ApplicationController
     order = Order.new(
       email: params[:stripeEmail],
       total_cents: cart_subtotal_cents,
-      stripe_charge_id: stripe_charge.id, # returned by stripe
+      stripe_charge_id: stripe_charge.id,
     )
 
     enhanced_cart.each do |entry|
