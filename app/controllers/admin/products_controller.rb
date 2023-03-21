3abc941d13dedc56fdb3b756002226c3075fc044
Hillary Forget <hillaryforget@outlook.com>
2023-03-21 02:33:07 -0400

    missing admin security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"], password: ENV["HTTP_ADMIN_PASS"]
+  
   def index
     @products = Product.order(id: :desc).all
   end
