7998c367faae48cf3024a3efa03648517b8ecab1
Hillary Forget <hillaryforget@outlook.com>
2023-03-21 03:40:18 -0400

    admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"], password: ENV["HTTP_ADMIN_PASS"]
-  
+
   def show
+    @product_count = Product.count
+    @categories_count = Category.count
   end
+
 end
